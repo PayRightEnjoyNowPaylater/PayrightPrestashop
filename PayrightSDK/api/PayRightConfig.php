c8088871e7a4901d1835a0c9005fd2b3ed592600
Neha Mendiratta <neha.mendiratta@payright.com.au>
2019-06-20 12:35:04 +1000

    [DEBUG] debug

--- PayrightSDK/api/PayRightConfig.php
+++ PayrightSDK/api/PayRightConfig.php
@@ -67,7 +67,7 @@ class PayRightConfig
     {
 
         $this->setConfigParams($payrightMode);
-        //$this->setEnvironment('dev');
+        $this->setEnvironment();
 
         /* if (empty($input)) {
     $filename = _PS_MODULE_DIR_.'payright/PayrightSDK/config/config.ini';
@@ -174,24 +174,23 @@ class PayRightConfig
         //Set Merchant Id, Secret Key and Callback Url
         //
        
-
-        if ($this->getMode() == 1) {
+       if ($this->getMode() == 1) {
            
-            /*$this->setAuthUrl('http://ecommerceapi.payright.local/oauth/token');
+            $this->setAuthUrl('http://ecommerceapi.payright.local/oauth/token');
             $this->setConfigUrl('http://ecommerceapi.payright.local/api/v1/configuration');
             $this->setIntialiseTransactionUrl('http://ecommerceapi.payright.local/api/v1/intialiseTransaction');
             $this->setEcomTokenDataUrl('http://ecommerceapi.payright.local/api/v1/getEcomTokenData');
             $this->setEcomUrl('http://customerpayrightportal.local/loan/new/');
             $this->setPlanStatusChangeUrl('http://ecommerceapi.payright.local/api/v1/changePlanStatus');
-            $this->setTransactionConfigUrl('http://ecommerceapi.payright.local/api/v1/initialTransactionConfiguration');*/
+            $this->setTransactionConfigUrl('http://ecommerceapi.payright.local/api/v1/initialTransactionConfiguration');
 
-            $this->setAuthUrl('https://api.payright.com.au/oauth/token');
+            /*$this->setAuthUrl('https://api.payright.com.au/oauth/token');
             $this->setConfigUrl('https://api.payright.com.au/api/v1/configuration');
             $this->setIntialiseTransactionUrl('https://api.payright.com.au/api/v1/intialiseTransaction');
             $this->setEcomTokenDataUrl('https://api.payright.com.au/api/v1/getEcomTokenData');
             $this->setEcomUrl('https://betaonline.payright.com.au/loan/new/');
             $this->setPlanStatusChangeUrl('https://api.payright.com.au/api/v1/changePlanStatus');
-            $this->setTransactionConfigUrl('https://api.payright.com.au/api/v1/initialTransactionConfiguration');
+            $this->setTransactionConfigUrl('https://api.payright.com.au/api/v1/initialTransactionConfiguration');*/
         } else {
             
             $this->setAuthUrl('https://liveapi.payright.com.au/oauth/token');
